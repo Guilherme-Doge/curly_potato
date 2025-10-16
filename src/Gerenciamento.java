3ca512145d920236178549ef6eeb84411d6ecf1a
Guilherme-Doge <guilherme_doge1@estudante.sesisenai.org.br>
2025-10-16 08:08:34 -0300

    Adição de comentário no Gerenciamento e troca de Hashmaps de Clientes e Agentes por Hashmap de Pessoas

--- src/Gerenciamento.java
+++ src/Gerenciamento.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Gerenciamento {
-    private Map<String, Cliente> clientes = new HashMap<>();
-    private Map<String, Agente> agentes = new HashMap<>();
+    private Map<String, Pessoa> clientes = new HashMap<>();
     private Map<String, Portal> portais = new HashMap<>();
     private List<Reserva> todasReservas = new ArrayList<>();
     private List<String> logs = new ArrayList<>();
     // métodos: menuPrincipal(), carregarDadosExemplo(), salvarLogs(), etc.
+    //métodos de crud dos maps
 }
